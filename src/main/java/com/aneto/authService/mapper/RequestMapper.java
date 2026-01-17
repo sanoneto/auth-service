0525623e00e1190a6f49f1b38c6d7a2e87b46ce4
sanoneto <143364088+sanoneto@users.noreply.github.com>
2026-01-17 02:11:09 +0000

    login no google 2

--- src/main/java/com/aneto/authService/mapper/RequestMapper.java
+++ src/main/java/com/aneto/authService/mapper/RequestMapper.java
@@ -7,10 +7,11 @@ import com.aneto.authService.models.Projects;
 import com.aneto.authService.models.Users;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface RequestMapper {
 
     @Mapping(target = "username", source = "username")
